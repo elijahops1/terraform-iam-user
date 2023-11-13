ed688f256b867e7d8a2cf090c997966593a96995
elijahops1 <141283145+elijahops1@users.noreply.github.com>
2023-11-13 09:00:22 -0500

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+access_key = "AKIAUPZR5PCMBRZA6SUH"
+secret_key = "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"
+region = "us-east-1"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "This is the access_key to the aws account"
+}
+
+variable "secret_key" {
+  description = "This is the secret_key to the aws account"
+}
+
+variable "region" {
+  description = "This is the region"
+}
